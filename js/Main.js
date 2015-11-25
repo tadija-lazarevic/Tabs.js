035b48053c754423b7a33471eec28077c6656296
lazarevic_tadija <lazarevic.tadija@gmail.com>
2015-11-25 01:03:56 +0100

    Changed module organization

--- js/Main.js
+++ js/Main.js
@@ -1,43 +1,23 @@
 function MainInit() {
-    var statusContent      = Utils.GetElementID('statusContent'),
-        photosContent      = Utils.GetElementID('photosContent'),
-        videoContent       = Utils.GetElementID('videoContent'),
-        audioContent       = Utils.GetElementID('audioContent'),
-        ulContent          = Utils.GetElementID('ulcontent'),
-        tadijaContent      = Utils.GetElementID('onemoretab'),
-        tabwithlinkContent = Utils.GetElementID('tabwithlink'),
-        tabWithLink        = Utils.GetElementID('tabWithLink'),
-        testTab            = Utils.GetElementID('testTab');
 
+    var accorditionArray    = [
+        {label: 'Business Tab', content: Utils.GetElementID('onemoretab'), isUL: true},
+        {label: 'Status Tab', content: Utils.GetElementID('status'), isUL: true},
+        {label: 'Customers Tab', content: Utils.GetElementID('customersTab'), isUL: true},
+        {label: 'Finances Tab', content: Utils.GetElementID('financesTab'), isUL: true}
+    ];
+    var accorditionSettings = {
+        appendTo: Utils.GetElementID('accorditionID'),
+        active: 'Finances Tab',
+        display : 'block',
+        position: 'relative',
+        tabs    : accorditionArray
+    };
+    var accorditionObj      = new Tabs();
+    accorditionObj.Init(accorditionSettings);
+//    accorditionObj.RemoveTab({label: 'Status Tab'})
 
-    var statusTab   = {label: 'Status Tab', href: '#status', content: statusContent};
-    var photosTab   = {label: 'Photos Tab', href: '#photos', content: photosContent, inactive: true};
-    var videoTab    = {label: 'Video Tab', href: '#video', content: videoContent};
-    var audioTab    = {label: 'Audio Tab', href: '#audio', content: audioContent};
-    var firstULTab  = {label: 'Some Ul Content', href: '#ulcontent', content: ulContent, isUL: true};
-    var secondULTab = {label: 'Tadija Tab', href: '#tadijacontent', content: tadijaContent, isUL: true, inactive: true};
-    var thirdULTab  = {label: 'Tab With Link', href: '#tabwithlink', content: tabwithlinkContent, isUL: true};
-    var duskoTab    = {label: 'Dusko Tab', href: '#duskotab', content: testTab};
-    var gagana      = {label: 'Gagana', href: '#gagana', content: audioContent};
-
-
-    var tabs = [];
-    tabs.push(statusTab, photosTab, videoTab, audioTab, firstULTab, secondULTab, thirdULTab);
-
-    var settings = {active: 'Status Tab', position: 'relative', display: 'inline', tabs: tabs};
-
-
-    // Init method
-    Tabs.Init(settings);
-
-    // Add new tab
-    Tabs.AppendTab(duskoTab);
-
-    // Removing
-    Tabs.RemoveTab(duskoTab);
-    Tabs.RemoveTab(thirdULTab);
-    Tabs.AppendTab(thirdULTab);
-    Tabs.RemoveTab(statusTab);
+    accorditionObj.AppendTab({label: 'Tadija'});
 
 
 }
--- js/Tabs.js
+++ js/Tabs.js
@@ -1,28 +1,27 @@
 var Tabs = (function () {
     'use strict';
 
-    // Global variables
-    var method          = {},
-        settings        = {},
-
-        tabULContainer  = document.createElement('ul'),
+    // Private fields
+    var tabULContainer  = document.createElement('ul'),
         tabHTMLContent  = [],
         tabULContent    = [],
         tabLinksArray   = [],
         tabLIArray      = [],
-        currentTab      = {},
         idPostfix       = 'ID',
         tabLIElement,
         aTag,
 
 
+        // Holder objects
+        settings        = {},
+        currentTab      = {},
         defaultSettings = {
             appendTo   : document.body,
-            before     : document.body.children[0],
             position   : 'fixed',
             active     : '',
             display    : 'block',
             tabs       : [],
+            ulID       : 'tabsID',
             showHTMLOn : 'click',
             showULOn   : 'click',
             hideULOn   : 'click',
@@ -30,7 +29,7 @@ var Tabs = (function () {
             hideChildOn: 'click',
             defaultTab : {
                 label   : 'new tab',
-                href    : '#newtab',
+                href    : '#',
                 inactive: false,
                 isUL    : false,
                 id      : 'newtabID',
@@ -38,51 +37,7 @@ var Tabs = (function () {
             }
         };
 
-
-    // Create tabs
-    method.Init = function (parameters) {
-        settings.appendTo    = parameters.appendTo || defaultSettings.appendTo;
-        settings.before      = parameters.before || defaultSettings.before;
-        settings.position    = parameters.position || defaultSettings.position;
-        settings.active      = parameters.active || defaultSettings.active;
-        settings.display     = parameters.display || defaultSettings.display;
-        settings.showHTMLOn  = parameters.showHTMLOn || defaultSettings.showHTMLOn;
-        settings.showULOn    = parameters.showULOn || defaultSettings.showULOn;
-        settings.hideULOn    = parameters.hideULOn || defaultSettings.hideULOn;
-        settings.showChildOn = parameters.showChildOn || defaultSettings.showChildOn;
-        settings.hideChildOn = parameters.hideChildOn || defaultSettings.hideChildOn;
-        settings.tabs        = parameters.tabs || defaultSettings.tabs;
-
-
-        // Sets ul element with id="tabs"
-        tabULContainer.setAttribute('id', 'tabs');
-
-        for (var i = 0; i < settings.tabs.length; i++) {
-            currentTab               = settings.tabs[i];
-            currentTab.tabIndex      = i;
-            currentTab.tabTitle      = currentTab.label;
-            currentTab.tabTitleLower = currentTab.tabTitle.toLowerCase();
-            currentTab.tabID         = currentTab.tabTitleLower.replace(/\s+/g, '') + idPostfix;
-            currentTab.tabHref       = currentTab.href.toLowerCase();
-
-            method.CreateLinks(currentTab);
-            if ((currentTab.content || currentTab.isUL) && !currentTab.ajaxContent) {
-                method.GetContent(currentTab);
-            }
-            else if (currentTab.ajaxContent && !currentTab.content) {
-
-            }
-            // Set style for single tab
-            method.SetTabStyle(currentTab);
-        }
-        // Set style for ul container
-        tabULContainer.style.position = settings.position;
-        Utils.AddClass(tabULContainer, 'tabs');
-    };
-
-    // Creates ul element with li's
-    method.CreateLinks = function (currentTab) {
-        // Creates link attribute
+    function CreateLinks(currentTab) {
         var aHref       = document.createElement('a');
         aHref.setAttribute('href', currentTab.tabHref);
         aHref.innerHTML = currentTab.tabTitle;
@@ -97,15 +52,13 @@ var Tabs = (function () {
 
         // Appends Tab UL to appendTo element before settings.before element
         settings.appendTo.insertBefore(tabULContainer, settings.before);
+    }
 
-    };
-
-    // Called if content is html
-    method.GetContent = function (currentTab) {
+    function GetContent(currentTab) {
         // Gets tabs child nodes
-        tabLIArray = Utils.GetElementID('tabs').childNodes;
+        tabLIArray = Utils.GetElementID(settings.ulID).childNodes;
 
-        aTag                                    = method.GetFirstChildWithTagName(tabLIArray[currentTab.tabIndex], 'a');
+        aTag                                    = GetFirstChildWithTagName(tabLIArray[currentTab.tabIndex], 'a');
         tabLinksArray[currentTab.tabTitleLower] = aTag;
         currentTab.content ? tabHTMLContent[currentTab.tabTitleLower] = currentTab.content : null;
         currentTab.isUL ? tabULContent[currentTab.tabTitleLower] = currentTab.content : null;
@@ -113,19 +66,17 @@ var Tabs = (function () {
         // On tab action
         if (tabLinksArray.hasOwnProperty(currentTab.tabTitleLower) && !currentTab.inactive) {
             if (!currentTab.isUL) {
-                Utils.GetElementID(currentTab.tabID).addEventListener(settings.showHTMLOn, method.ShowHTMLContent);
+                Utils.GetElementID(currentTab.tabID).addEventListener(settings.showHTMLOn, ShowHTMLContent);
             }
             else if (currentTab.isUL) {
-                Utils.GetElementID(currentTab.tabID).addEventListener(settings.showULOn, method.ShowULContent);
+                Utils.GetElementID(currentTab.tabID).addEventListener(settings.showULOn, ShowULContent);
             }
         }
+    }
 
-    };
-
-    // Show HTML content
-    method.ShowHTMLContent = function () {
+    function ShowHTMLContent() {
         // Gets the name of selected tab
-        var selectedTab = method.ReturnSelected();
+        var selectedTab = ReturnSelected();
 
         for (var id in tabHTMLContent) {
             if (id === selectedTab.selected) {
@@ -144,11 +95,10 @@ var Tabs = (function () {
                 }
             }
         }
-    };
+    }
 
-    // Show ul content
-    method.ShowULContent = function () {
-        var selectedTab = method.ReturnSelected();
+    function ShowULContent() {
+        var selectedTab = ReturnSelected();
 
         for (var ulTab in tabULContent) {
             if (ulTab === selectedTab.selected) {
@@ -171,7 +121,7 @@ var Tabs = (function () {
                     // Check for child elements of li
                     for (var i = 0; i < liChildes.length; i++) {
                         currentChild   = liChildes[i];
-                        currentChildUL = method.GetFirstChildWithTagName(currentChild, 'ul');
+                        currentChildUL = GetFirstChildWithTagName(currentChild, 'ul');
 
                         if (currentChildUL !== undefined) {
                             Utils.AddClass(currentChildUL, 'ulHide');
@@ -192,10 +142,9 @@ var Tabs = (function () {
                 }
             }
         }
-    };
+    }
 
-    // Initialize tabs style, default tab, inactive tab
-    method.SetTabStyle = function (currentTab) {
+    function SetTabStyle() {
 
         // Hides all tabs content at the start
         currentTab.isUL ? Utils.AddClass(tabULContent[currentTab.tabTitleLower], 'tabDropdownHide') :
@@ -221,18 +170,15 @@ var Tabs = (function () {
 
         Utils.AddClass(tabLIArray[currentTab.tabIndex], 'tabsLiElement');
         tabLIArray[currentTab.tabIndex].style.display = settings.display;
+    }
 
-    };
-
-    // Get first child with tag name
-    method.GetFirstChildWithTagName = function (element, tagName) {
+    function GetFirstChildWithTagName(element, tagName) {
         for (var i = 0; i < element.childNodes.length; i++) {
             if (element.childNodes[i].nodeName.toLowerCase() == tagName) return element.childNodes[i];
         }
-    };
+    }
 
-    // Return clicked item
-    method.ReturnSelected = function (e) {
+    function ReturnSelected(e) {
         var elem,
             evt = e ? e : event;
         if (evt.srcElement) {
@@ -244,58 +190,101 @@ var Tabs = (function () {
         return {
             selected: elem.innerText.toLowerCase()
         };
-    };
+    }
 
-    // Adds new tab
-    method.AppendTab = function (parametars) {
-        var newTab = {
-            label   : parametars.label || defaultSettings.defaultTab.label,
-            href    : parametars.href || defaultSettings.defaultTab.href,
-            content : parametars.content || defaultSettings.defaultTab.content,
-            isUL    : parametars.isUL || defaultSettings.defaultTab.isUL,
-            inactive: parametars.inactive || defaultSettings.defaultTab.inactive,
-            id      : parametars.id + idPostfix || defaultSettings.defaultTab.id
-        };
+    // Public methods
+    return {
 
-        currentTab               = newTab;
-        currentTab.tabTitle      = newTab.label;
-        currentTab.tabTitleLower = currentTab.tabTitle.toLowerCase();
-        currentTab.tabID         = currentTab.tabTitleLower.replace(/\s+/g, '') + idPostfix;
-        currentTab.tabHref       = newTab.href.toLowerCase();
-        currentTab.tabIndex      = settings.tabs.length;
+        Init: function (parameters) {
+            settings.appendTo    = parameters.appendTo || defaultSettings.appendTo;
+            settings.before      = parameters.before || settings.appendTo.children[0];
+            settings.position    = parameters.position || defaultSettings.position;
+            settings.active      = parameters.active || defaultSettings.active;
+            settings.display     = parameters.display || defaultSettings.display;
+            settings.showHTMLOn  = parameters.showHTMLOn || defaultSettings.showHTMLOn;
+            settings.showULOn    = parameters.showULOn || defaultSettings.showULOn;
+            settings.hideULOn    = parameters.hideULOn || defaultSettings.hideULOn;
+            settings.showChildOn = parameters.showChildOn || defaultSettings.showChildOn;
+            settings.hideChildOn = parameters.hideChildOn || defaultSettings.hideChildOn;
+            settings.tabs        = parameters.tabs || defaultSettings.tabs;
+            settings.ulID        = parameters.ulID || defaultSettings.ulID;
 
-        //Do some stuff for new tab, create link, get content and set styling
-        method.CreateLinks(currentTab);
-        method.GetContent(currentTab);
-        method.SetTabStyle(currentTab);
 
-        // Add new tab to tabs array
-        settings.tabs.push(newTab);
-
-    };
-
-    // Remove tab by id
-    method.RemoveTab = function (parametars) {
-        if (parametars) {
-            var removeLabel = parametars.label,
-                removeTab   = {},
-                index       = 0;
+            // Sets ul element with id="tabs"
+            tabULContainer.setAttribute('id', settings.ulID);
 
             for (var i = 0; i < settings.tabs.length; i++) {
-                removeTab = settings.tabs[i];
-                if (removeLabel === removeTab.label) {
-                    index = removeTab.tabIndex;
-                    if (index > -1) {
-                        // Remove element from tabs array
-                        settings.tabs.splice(index, 1);
-                        // Remove link from links array
-                        tabLIArray[index].outerHTML = '';
-                    }
-                }
+                currentTab               = settings.tabs[i];
+                currentTab.tabIndex      = i;
+                currentTab.tabTitle      = currentTab.label || defaultSettings.defaultTab.label;
+                currentTab.tabTitleLower = currentTab.tabTitle.toLowerCase();
+                currentTab.tabID         = currentTab.tabTitleLower.replace(/\s+/g, '') + idPostfix;
+                currentTab.tabHref       = currentTab.href || defaultSettings.defaultTab.href;
+                currentTab.inactive      = currentTab.inactive || defaultSettings.defaultTab.inactive;
+                currentTab.isUL          = currentTab.isUL || defaultSettings.defaultTab.isUL;
 
+                CreateLinks(currentTab);
+                if ((currentTab.content || currentTab.isUL) && !currentTab.ajaxContent) {
+                    GetContent(currentTab);
+                }
+                else if (currentTab.ajaxContent && !currentTab.content) {
+
+                }
+                // Set style for single tab
+                SetTabStyle(currentTab);
+            }
+            // Set style for ul container
+            tabULContainer.style.position = settings.position;
+            Utils.AddClass(tabULContainer, 'tabs');
+        },
+
+        AppendTab: function (params) {
+            var newTab = {
+                label   : params.label || defaultSettings.defaultTab.label,
+                href    : params.href || defaultSettings.defaultTab.href,
+                content : params.content || defaultSettings.defaultTab.content,
+                isUL    : params.isUL || defaultSettings.defaultTab.isUL,
+                inactive: params.inactive || defaultSettings.defaultTab.inactive,
+                id      : params.id + idPostfix || defaultSettings.defaultTab.id
+            };
+
+            currentTab               = newTab;
+            currentTab.tabTitle      = newTab.label;
+            currentTab.tabTitleLower = currentTab.tabTitle.toLowerCase();
+            currentTab.tabID         = currentTab.tabTitleLower.replace(/\s+/g, '') + idPostfix;
+            currentTab.tabHref       = newTab.href.toLowerCase();
+            currentTab.tabIndex      = settings.tabs.length;
+
+            //Do some stuff for new tab, create link, get content and set styling
+            CreateLinks(currentTab);
+            GetContent(currentTab);
+            SetTabStyle(currentTab);
+
+            // Add new tab to tabs array
+            settings.tabs.push(newTab);
+        },
+
+        RemoveTab: function (params) {
+            if (params) {
+                var removeLabel = params.label,
+                    removeTab   = {},
+                    index       = 0;
+
+                for (var i = 0; i < settings.tabs.length; i++) {
+                    removeTab = settings.tabs[i];
+                    if (removeLabel === removeTab.label) {
+                        index = removeTab.tabIndex;
+                        if (index > -1) {
+                            // Remove element from tabs array
+                            settings.tabs.splice(index, 1);
+                            // Remove link from links array
+                            tabLIArray[index].remove();
+                        }
+                    }
+
+                }
             }
         }
     };
 
-    return method;
-}());
+});
